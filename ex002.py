d88af53e52ac1853da20ff8e321be9dc584cb671
Rickypython <87348741+Rickypython@users.noreply.github.com>
2021-08-11 19:57:30 -0300

    Add files via upload

---
+++ ex002.py
@@ -0,0 +1,2 @@
+nome=input('digite seu nome')
+print('muito prazer', nome)
---
+++ ex003.py
@@ -0,0 +1,10 @@
+n1= int(input('digite um número:'))
+n2= int(input('digite um número:'))
+s=n1+n2
+#print('a soma entre',n1,'e', n2,'vale',s)  QUANDO COLOCA # fica só como comentario
+print('a soma entre {} e {} é vale {}'.format(n1,n2, s)) 
+
+
+
+
+
