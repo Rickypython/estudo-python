33dd57ce98c0e7b996ca1ead58fb803dadd9edcd
Luiz Ricardo <87348741+Rickypython@users.noreply.github.com>
2021-08-12 18:25:37 -0300

    Add files via upload

---
+++ ex004.py
@@ -0,0 +1,11 @@
+a = input('digite algo')
+print('o tipo primitivo desse valor é', type(a))
+print('só tem espaços?',a.isspace())
+#esse de cima é para saber se só foi digitado espaço.
+print('é um número?', a.isnumeric())
+print('é alfabético?', a.isalpha())
+print('é alfanumerico?', a.isalnum())
+print('esta em maiuscula?', a.isupper())
+print('está em minusculo?', a.islower())
+print('está Capitalizado?', a.istitle())
+#esse é para saber se esta em maiuscula e minuscula
---
+++ ex005.py
@@ -0,0 +1,8 @@
+#faça um programa que leia um número inteiro e mostre na tela o seu sucessor e antecessor
+n = int(input('digite um número'))
+a = n - 1
+s = n + 1
+print('o antecessor do número{}é {},e seu sucessor é {}'.format(n,a ,s))
+
+
+
